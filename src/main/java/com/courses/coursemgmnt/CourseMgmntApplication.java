ff45714e729c161b279c2f0746c5f8fc2536217f
Ritu <ritu@ritu-HP-Pavilion-Notebook>
2020-09-06 15:15:08 +0530

    initial set-up

---
+++ src/main/java/com/courses/coursemgmnt/CourseMgmntApplication.java
@@ -0,0 +1,13 @@
+package com.courses.coursemgmnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMgmntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMgmntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/courses/coursemgmnt/CourseMgmntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courses.coursemgmnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMgmntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
